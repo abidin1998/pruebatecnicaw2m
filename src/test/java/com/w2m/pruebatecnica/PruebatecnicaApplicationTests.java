024826ceddd9c17b07f95636e9021781de4411ac
abidin1998 <38974414+abidin1998@users.noreply.github.com>
2022-05-10 20:46:33 +0200

    Initial commit

---
+++ src/test/java/com/w2m/pruebatecnica/PruebatecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w2m.pruebatecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebatecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
